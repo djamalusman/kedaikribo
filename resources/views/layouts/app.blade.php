d18c2d32e15ba666c9eb250a2d4ecb9e3ff0fad5
Dj <jiiusman@gmail.com>
2025-12-22 02:57:37 +0700

    oks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -231,7 +231,7 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2023 &copy; Kribo</p>
+                        <p>2026 &copy; Kribo</p>
                     </div>
                     {{-- <div class="float-end">
                         <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
