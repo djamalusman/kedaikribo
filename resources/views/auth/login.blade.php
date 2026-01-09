4856dc98adac6bf1e518c75638099c02ad5d566d
Dj <jiiusman@gmail.com>
2026-01-09 22:16:05 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
 
         <div class="info-text login">
             <h2 class="animation" style="--i:0; --j:20">Welcome Back!</h2>
-            <p class="animation" style="--i:1; --j:21">enjoy your business</p>
+            <p class="animation" style="--i:1; --j:21">"Enjoy Your Business"</p>
         </div>
     </div>
     <!--Script.js-->
